819f245c8ad21bce398aedfdbc5e94672a27d227
'csprashant' <prashant.puse@innodeed.com>
2023-11-29 12:52:36 +0530

    intial commit

---
+++ src/main/java/com/inno/Demo1.java
@@ -0,0 +1,13 @@
+package com.inno;
+
+public class Demo1 {
+private Integer id;
+private String name;
+private Boolean actived;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
