c4df8706a303c43f63375a2365dbd3e153ac3856
'csprashant' <prashant.puse@innodeed.com>
2023-11-29 13:05:10 +0530

    sdfs

--- src/main/java/com/inno/Demo1.java
+++ src/main/java/com/inno/Demo1.java
@@ -4,6 +4,7 @@ public class Demo1 {
 private Integer id;
 private String name;
 private Boolean actived;
+private Boolean thread;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
